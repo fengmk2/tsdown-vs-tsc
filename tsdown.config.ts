f6ffe46dd5077dd81b13ad9584d7972228fbbba8
MK <fengmk2@gmail.com>
2025-10-04 23:28:27 +0800

    FIXUP

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
   entry: 'src/**/*.ts',
   dts: true,
+  // FIXME: unbundle is not working as expected
   unbundle: true,
   unused: {
     level: 'error',
